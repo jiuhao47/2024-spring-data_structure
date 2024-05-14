1005e900466da2a476780f8657883739bc2b0e95
Junyan Jiang <jiangjunyan22@mails.ucas.ac.cn>
2024-05-14 18:05:03 +0800

    2024-05-14

---
+++ Ex/projects/JPEG/build/CMakeFiles/JPEG.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JPEG.dir/home/jiuhao/Workspace/2024-Spring/data_structure/Ex/projects/JPEG/src/main.c.o"
+  "CMakeFiles/JPEG.dir/home/jiuhao/Workspace/2024-Spring/data_structure/Ex/projects/JPEG/src/main.c.o.d"
+  "JPEG"
+  "JPEG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JPEG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ex/projects/JPEG/build/CMakeFiles/JPEG.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for JPEG.
+# This may be replaced when dependencies are built.
